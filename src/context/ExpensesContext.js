ecdb66b8bcdd5aea33961e2bba6c373087b95f5e
leo <leocsdev@gmail.com>
2021-12-18 18:54:27 +0800

    Added editExpense and updateExpense functions.

--- src/context/ExpensesContext.js
+++ src/context/ExpensesContext.js
@@ -8,13 +8,35 @@ const ExpensesContext = createContext()
 export const ExpensesProvider = ({ children }) => {
   const [expenses, setExpenses] = useState(expensesData)
 
+  // initialize expense to edit
+  const [expenseToEdit, setExpenseToEdit] = useState({
+    expense: {},
+    edit: false,
+  })
+
   // Add expense
   const addExpense = (expense) => {
     expense.id = uuidv4()
     setExpenses([expense, ...expenses])
   }
 
+  // Edit expense
+  // Set the expense item to update and render to form
+  // expense coming from the clicked edit icon
+  const editExpense = (expense) => {
+    console.log(`Edit item: ${expense}`)
+
+    setExpenseToEdit({ expense, edit: true })
+  }
+
   // Update expense
+  const updateExpense = (id, updExpense) => {
+    setExpenses(
+      expenses.map((expense) =>
+        expense.id === id ? { ...expense, ...updExpense } : expense
+      )
+    )
+  }
 
   // Delete expense
   const deleteExpense = (id) => {
@@ -32,7 +54,16 @@ export const ExpensesProvider = ({ children }) => {
 
   return (
     <ExpensesContext.Provider
-      value={{ expenses, addExpense, currencyFormat, deleteExpense }}
+      value={{
+        expenses,
+        expenseToEdit,
+        setExpenseToEdit,
+        addExpense,
+        editExpense,
+        updateExpense,
+        deleteExpense,
+        currencyFormat,
+      }}
     >
       {children}
     </ExpensesContext.Provider>
