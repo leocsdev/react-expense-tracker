771643800ea4a345f5426bd5e4c83cb43b0af119
leo <leocsdev@gmail.com>
2021-12-18 11:33:37 +0800

    Added deleteExpense function in expenseContext.

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,24 +2,26 @@ import { useContext } from 'react'
 import ExpensesContext from '../context/ExpensesContext'
 
 import { Col, ListGroup, Row } from 'react-bootstrap'
+import { FaTimes } from 'react-icons/fa'
 
 function ExpenseItem({ expense }) {
-  const { currencyFormat } = useContext(ExpensesContext)
-
-  // // Currency Helper
-  // const currencyFormat = (num) => {
-  //   return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-  // }
+  const { currencyFormat, deleteExpense } = useContext(ExpensesContext)
 
   return (
     <ListGroup.Item>
       <Row>
         <Col className='d-none d-sm-block'>{expense.date}</Col>
-        <Col sm={6} className='text-center text-sm-start'>
+        <Col sm={4} className='text-center text-sm-start'>
           {expense.item}
         </Col>
         <Col className='text-center text-sm-end'>
-          {currencyFormat(expense.amount)}
+          {currencyFormat(expense.amount)}{' '}
+          <span
+            style={{ display: 'inline-block', paddingLeft: '0.5rem' }}
+            onClick={() => deleteExpense(expense.id)}
+          >
+            <FaTimes color='red' size={14} />
+          </span>
         </Col>
       </Row>
     </ListGroup.Item>
--- src/context/ExpensesContext.js
+++ src/context/ExpensesContext.js
@@ -1,4 +1,6 @@
 import { createContext, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
 import expensesData from '../data/expensesData'
 
 const ExpensesContext = createContext()
@@ -6,9 +8,21 @@ const ExpensesContext = createContext()
 export const ExpensesProvider = ({ children }) => {
   const [expenses, setExpenses] = useState(expensesData)
 
-  const addExpense = (expenseData) => {
-    // add expense
-    setExpenses([expenseData, ...expenses])
+  // Add expense
+  const addExpense = (expense) => {
+    expense.id = uuidv4()
+    setExpenses([expense, ...expenses])
+  }
+
+  // Update expense
+
+  // Delete expense
+  const deleteExpense = (id) => {
+    console.log(`Delete id: ${id}`)
+
+    if (window.confirm('Are you sure you want to delete an item?')) {
+      setExpenses(expenses.filter((expense) => expense.id !== id))
+    }
   }
 
   // Currency Format Helper
@@ -17,7 +31,9 @@ export const ExpensesProvider = ({ children }) => {
   }
 
   return (
-    <ExpensesContext.Provider value={{ expenses, addExpense, currencyFormat }}>
+    <ExpensesContext.Provider
+      value={{ expenses, addExpense, currencyFormat, deleteExpense }}
+    >
       {children}
     </ExpensesContext.Provider>
   )
